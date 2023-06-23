512aee42fbc2a21c54d6e9573748ced1c709ff76
ahmadileham <ilham130602@gmail.com>
2023-06-23 09:42:49 +0800

    d

--- resources/views/h/home.blade.php
+++ resources/views/h/home.blade.php
@@ -3,9 +3,15 @@
 @section('title', 'Home')
 
 @section('content')
-            <div class="pin_container">
-                @foreach($posts as $key=>$post)
-                    <div class="card card_medium"><a href='{{ url("post/details/$post->id") }}'><img src="{{ asset($post->image) }}" alt="Post Image"></a></div>
-                @endforeach
-            </div>
+            <div style="height: 100%;" class="pin_container">
+        @if ($posts->isEmpty())
+        <div>
+            <h3 style="padding-top: 10%;">No posts to show.</h3>
+        </div>
+        @else
+            @foreach($posts as $key => $post)
+                <div class="card card_medium"><a href='{{ url("post/details/$post->id") }}'><img src="{{ asset($post->image) }}" alt="Post Image"></a></div>
+            @endforeach
+        @endif
+    </div>
 @endsection
